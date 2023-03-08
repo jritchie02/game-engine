ae7a9bc1376cd4adcf67756922215f509beb6b85
jritchie02 <jaredr_22@yahoo.com>
2023-03-08 13:43:11 -0500

    Small SFML update

--- main.cpp
+++ main.cpp
@@ -2,7 +2,7 @@
 
 int main()
 {
-    sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
+    sf::RenderWindow window(sf::VideoMode(400, 400), "SFML Window!");
     sf::CircleShape shape(100.f);
     shape.setFillColor(sf::Color::Green);
 
